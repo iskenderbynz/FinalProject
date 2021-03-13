81dcac57e64f9f1ef7039d803e43720adef8d967
Iskender Boynuz <iskenderboynuz90@hotmail.com>
2021-03-13 23:52:44 +0300

    13.03.2021 ders sonu

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -45,6 +45,8 @@ namespace WebAPI
             //services.AddSingleton<IProductService,ProductManager>();
             //services.AddSingleton<IProductDal, EfProductDal>();
 
+            services.AddCors();
+
             var tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();
 
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
@@ -74,7 +76,7 @@ namespace WebAPI
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            app.UseCors(builder=>builder.WithOrigins("http://localhost:4200").AllowAnyHeader());
             app.UseHttpsRedirection();
 
             app.UseRouting();
